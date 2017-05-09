fc8f1fc542e8f2849cce915f89748bda312fd69f
davron028 <davron.rakhmatullaev@gmail.com>
2017-05-10 02:36:27 +0500

    add /big

---
+++ checkValidation.php
@@ -0,0 +1,48 @@
+<?php 
+include "connection.php";
+
+$fullnameErr = $emailErr = $genderErr = $passwordErr = "";
+$fullname = $email = $gender = $password = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  if (empty($_POST["fullname"])) {
+    $nameErr = "Name is required";
+  } else {
+    $name = test_input($_POST["name"]);
+    // check if name only contains letters and whitespace
+    if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
+      $nameErr = "Only letters and white space allowed"; 
+    }
+  }
+  
+  if (empty($_POST["email"])) {
+    $emailErr = "Email is required";
+  } else {
+    $email = test_input($_POST["email"]);
+    // check if e-mail address is well-formed
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      $emailErr = "Invalid email format"; 
+    }
+  }
+  
+  if ($_POST["password"] == $_POST["confirm_password"]) {
+   $password = "Correct";
+  }
+  else {
+   $passwordErr = "You wrote two different password";
+  }
+
+  if (empty($_POST["gender"])) {
+    $genderErr = "Gender is required";
+  } else {
+    $gender = test_input($_POST["gender"]);
+  }
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+ ?>
